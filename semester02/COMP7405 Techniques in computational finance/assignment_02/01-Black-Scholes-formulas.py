5511675ff44ac7ecaefbe761c6f64edfa7c398b6
Varis Darasirikul <varis.dara@gmail.com>
2021-03-11 00:58:43 +0800

    feature/COMP-7405-assignment-02: Prepare dn function

--- semester02/COMP7405 Techniques in computational finance/assignment_02/01-Black-Scholes-formulas.py
+++ semester02/COMP7405 Techniques in computational finance/assignment_02/01-Black-Scholes-formulas.py
@@ -1,9 +1,25 @@
+import numpy as np
+
 # Data
+# σ = sigma
+# δ = delta
 # S = 50, K = 50, t = 0, T = 0:5, sigma = 20%, and r = 1%.
 # S = 50, K = 60, t = 0, T = 0:5, sigma = 20%, and r = 1%.
 # S = 50, K = 50, t = 0, T = 1:0, sigma = 20%, and r = 1%.
 # S = 50, K = 50, t = 0, T = 0:5, sigma = 30%, and r = 1%.
 # S = 50, K = 50, t = 0, T = 0:5, sigma = 20%, and r = 2%.
+
+def d1(S,K,t,T,r,sigma):
+  # d1 = ((ln(S / K) + (r + (sigma^2 / 2)) * (T - t)) / (sigma * √T-t)
+  time = T - t
+  lnSK = np.log(S / K)
+  rate = (r + (np.power(sigma, 2) / 2)) * (time)
+  denominator = sigma * (np.sqrt(time))
+  return (lnSK + rate) / denominator
+
+def d2(S,K,t,T,r,sigma):
+  # d2 = d1 - sigma * √T-t
+  return d1(S,K,t,T,r,sigma) - (sigma * np.sqrt(T - t))
 
 def black_scholes(S, K, t, T, sigma, r):
   # Black Scholes Formula
@@ -12,10 +28,7 @@ def black_scholes(S, K, t, T, sigma, r):
   # C(S,t) = SN(d1) - Ke^(-r(T - t)) * N(d2)
 
   # Put Option
-  # P(S,t) = Ke^(-r(T - t)) * N(-d2) - SN(-d1)
-
-  # d1 = (ln(S / K) + r(T - t)) / (sigma * √T) + ((1/2) * sigma * √T-t)
-  # d2 = (ln(S / K) + r(T - t)) / (sigma * √T) - ((1/2) * sigma * √T-t)
+  # P(S,t) = Ke^(-r(T - t)) * N(-d2) - SN(-d1)  
 
   # S = Current stock price
   # N(d1) and N(d2) = Cumulative density function
