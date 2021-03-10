4b0eca4f7d1e033d07d7315f98274285f922c481
Varis Darasirikul <varis.dara@gmail.com>
2021-03-10 23:54:27 +0800

    master: Add COMP 7405 assignment 2

---
+++ semester02/COMP7405 Techniques in computational finance/assignment_02/01-Black-Scholes-formulas.py
@@ -0,0 +1,13 @@
+# Data
+# S = 50, K = 50, t = 0, T = 0:5,  = 20%, and r = 1%.
+# S = 50, K = 60, t = 0, T = 0:5,  = 20%, and r = 1%.
+# S = 50, K = 50, t = 0, T = 1:0,  = 20%, and r = 1%.
+# S = 50, K = 50, t = 0, T = 0:5,  = 30%, and r = 1%.
+# S = 50, K = 50, t = 0, T = 0:5,  = 20%, and r = 2%.
+
+def black_scholes():
+  # Black Scholes Formula
+  # C(S,t) = SN(d1) - Ke^(-r(T - t))N(d2)
+  # S = Current stock price
+
+  return None
